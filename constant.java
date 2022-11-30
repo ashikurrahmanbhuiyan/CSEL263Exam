fbf711a8fb74e61cd8a5cec440e4e60ce05aa05d
ashik <s2010376107@ru.ac.bd>
2022-11-30 08:22:36 +0600

    ok

--- constant.java
+++ constant.java
@@ -1,11 +1,18 @@
-public class constant {
-    public static String comma = "a";
-    public static String separator = ", ";
-    public static String loading = "Loading data ...";
-    public static String textFile = "students.txt";
-    public static String endMessage = "Data Loaded.";
+public class constant{
     public static String inputFileName = "students.txt";
     public static String errorMessage = "Usage:  a|r|c|+word|?word";
     public static String displayCommand = "a";
     public static String addCommand = "+";
+    public static String searchCommand = "?";
+    public static String countCommand = "c";
+    public static String randomAccessCommand = "r";
+    public static String waitingMessage = "Loading data ...";
+    public static String endMessage = "Data Loaded.";
+    public static String separator = ",";
+    public static String timeFormat = "dd/mm/yyyy-hh:mm:ss a";
+    public static String singleSpace = " ";
+    public static char singleSpaceChar = ' ';
+    public static String updateMessage = "\nList last updated on ";
+    public static String foundMessage = "We found it!";
+    public static String countMessage = " word(s) found ";
 }
