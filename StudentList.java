13e0015dc4660a0e4467721daabdef94e4d0ca00
ashik <s2010376107@ru.ac.bd>
2022-11-30 07:44:18 +0600

    ok

--- StudentList.java
+++ StudentList.java
@@ -7,33 +7,33 @@ public class StudentList {
 		if (args.length == 0) {
 			System.out.println("Please put a argument and try again.");
 		} else {
-			if (args[0].equals("a")) {
-				System.out.println("Loading data ...");
+			if (args[0].equals(constant.comma)) {
+				System.out.println(constant.loading);
 				try {
-					String i[] = students().split(", ");
+					String i[] = students().split(constant.separator);
 					for (String j : i) {
 						System.out.println(j);
 					}
 					
 				} catch (Exception e) {
 				}
-				System.out.println("Data Loaded.");
+				System.out.println(constant.endMessage);
 			} else if (args[0].equals("r")) {
-				System.out.println("Loading data ...");
+				System.out.println(constant.loading);
 				try {
-					String i[] = students().split(",");
+					String i[] = students().split(constant.separator);
 					Random x = new Random();
 					System.out.println(i[x.nextInt(4)]);
 				} catch (Exception e) {
 				}
 				System.out.println("Data Loaded.");
 			} else if (args[0].contains("+")) {
-				System.out.println("Loading data ...");
+				System.out.println(constant.loading);
 				try {
 					String i = students();
-					File file = new File("students.txt");
+					File file = new File(constant.textFile);
 					file.delete();
-					File file1 = new File("students.txt");
+					File file1 = new File(constant.textFile);
 					file1.createNewFile();
 					BufferedWriter s = studentName();
 					s.write(i);
@@ -43,11 +43,11 @@ public class StudentList {
 				} catch (Exception e) {
 				}
 
-				System.out.println("Data Loaded.");
+				System.out.println(constant.endMessage);
 			} else if (args[0].contains("?")) {
-				System.out.println("Loading data ...");
+				System.out.println(constant.loading);
 				try {
-					String i[] = students().split(", ");
+					String i[] = students().split(constant.separator);
 					for (int idx = 0; idx < i.length; idx++) {
 						if (i[idx].equals(args[0].substring(1))) {
 							System.out.println("We found it!");
@@ -56,9 +56,9 @@ public class StudentList {
 					}
 				} catch (Exception e) {
 				}
-				System.out.println("Data Loaded.");
+				System.out.println(constant.endMessage);
 			} else if (args[0].contains("c")) {
-				System.out.println("Loading data ...");
+				System.out.println(constant.loading);
 				try {
 					char a[] = students().toCharArray();
 					boolean in_word = false;
@@ -76,7 +76,7 @@ public class StudentList {
 					System.out.println(count + " word(s) found " + a.length);
 				} catch (Exception e) {
 				}
-				System.out.println("Data Loaded.");
+				System.out.println(constant.endMessage);
 			} else {
 				System.out.println("please put a valid argument and try again");
 			}
@@ -93,8 +93,8 @@ public class StudentList {
 		BufferedReader reader = new BufferedReader(
 				new InputStreamReader(
 						new FileInputStream("students.txt")));
-		String r = reader.readLine();
+		String names = reader.readLine();
 		reader.close();
-		return r;
+		return names;
 	}
 }
---
+++ constant.java
@@ -0,0 +1,11 @@
+public class constant {
+    public static String comma = "a";
+    public static String separator = ", ";
+    public static String loading = "Loading data ...";
+    public static String textFile = "students.txt";
+    public static String endMessage = "Data Loaded.";
+    public static String inputFileName = "students.txt";
+    public static String errorMessage = "Usage:  a|r|c|+word|?word";
+    public static String displayCommand = "a";
+    public static String addCommand = "+";
+}
